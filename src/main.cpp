30cc31ffd6033184e51ba9a245277c2fa21e2eca
Aitor Forero <aitor.forero@gmail.com>
2024-01-03 19:21:35 +0100

    Paso a usar la libreria NimBLE-Arduino. Fallaba al conectarse con Android y ahora ya no

--- src/main.cpp
+++ src/main.cpp
@@ -132,14 +132,14 @@ void batteryCheck()
 
 void remove_all_bonded_devices(void)
 {
-    int dev_num = esp_ble_get_bond_device_num();
-    esp_ble_bond_dev_t *dev_list = (esp_ble_bond_dev_t *)malloc(sizeof(esp_ble_bond_dev_t) * dev_num);
-    esp_ble_get_bond_device_list(&dev_num, dev_list);
-    for (int i = 0; i < dev_num; i++) {
-        esp_ble_remove_bond_device(dev_list[i].bd_addr);
-    }
+    // int dev_num = esp_ble_get_bond_device_num();
+    // esp_ble_bond_dev_t *dev_list = (esp_ble_bond_dev_t *)malloc(sizeof(esp_ble_bond_dev_t) * dev_num);
+    // esp_ble_get_bond_device_list(&dev_num, dev_list);
+    // for (int i = 0; i < dev_num; i++) {
+    //     esp_ble_remove_bond_device(dev_list[i].bd_addr);
+    // }
 
-    free(dev_list);
+    // free(dev_list);
 }
 
 
